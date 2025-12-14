c5d387b212bb051252809d71fd255e5a9f59e783
Pavogani <Kexoti@yahoo.com>
2025-12-14 14:53:56 -0600

    Add_missing_custom_view_classes

---
+++ android/app/src/main/java/com/derakus/legends/TouchActionButton.kt
@@ -0,0 +1,11 @@
+package com.derakus.legends
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class TouchActionButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, defStyleAttr)
